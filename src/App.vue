c0566dd9199d79cc91a7de288171d77185fc079e
Braulio95 <braulioestradaalfonso@hotmail.com>
2023-12-10 01:03:24 -0600

    first project on vue js

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navbar
+    :pages="pages"
+    :active-page="activePage"
+    :nav-link-click="(index)=>activePage = index"
+    ></navbar>
+    <page-viewer
+    :page="pages[activePage]"
+    ></page-viewer>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+import PageViewer from './components/PageViewer.vue';
+import Navbar from './components/Navbar.vue';
+    export default{
+        components:{
+            Navbar,
+            PageViewer
+        },
+        data(){
+            return{
+                activePage:0,
+                pages: [
+                    {
+                        link: {text:'Home',url:'index.hmtl'},
+                        pageTitle: 'Home Page',
+                        content: 'This is the home content'
+                    },
+                    {
+                        link: {text:'About',url:'about.hmtl'},
+                        pageTitle: 'About Page',
+                        content: 'This is the About content'
+                    },
+                    {
+                        link: {text:'Contact',url:'contact.hmtl'},
+                        pageTitle: 'Contact Page',
+                        content: 'This is the Contact content'
+                    }
+                ]
+            }
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
