233dfb58236c6e760b8b7fc80cac7be3213660f2
Braulio95 <braulioestradaalfonso@hotmail.com>
2023-12-11 13:57:01 -0600

    new lesson

--- src/components/CreatePage.vue
+++ src/components/CreatePage.vue
@@ -1,84 +1,101 @@
 <template>
-    <div class="container mb-3">
-        <form action="">
-            <div class="mb-3">
-                <label for="" class="form-label"> Page Title </label>
-                <input type="text" class="form-control" v-model="pageTitle" />
-            </div>
-            <div class="mb-3">
-                <label for="" class="form-label"> Content </label>
-                <textarea
-                    type="text"
-                    class="form-control"
-                    rows="5"
-                    v-model="content"
-                ></textarea>
-            </div>
-            <div class="col">
-                <div class="mb-3">
-                    <label for="" class="form-label">Link text</label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        v-model="linkText"
-                    />
-                </div>
-                <div class="mb-3">
-                    <label for="" class="form-label"> Link URL </label>
-                    <input type="text" class="form-control" v-model="linkUrl" />
-                </div>
-                <div class="row mb-3">
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" />
-                        <label for="gridCheck1" class="form-check-label"
-                            >Published
-                        </label>
-                    </div>
-                </div>
-            </div>
-            <div class="mb-3">
-                <button
-                    class="btn btn-primary"
-                    :disabled="isFormValid"
-                    @click.prevent="
-                        pageCreated({ pageTitle, content, linkText, linkUrl })
-                    "
-                >
-                    Create Page
-                </button>
-            </div>
-        </form>
-    </div>
+  <div class="container mb-3">
+    <form action="">
+      <div class="mb-3">
+        <label for="" class="form-label"> Page Title </label>
+        <input type="text" class="form-control" v-model="pageTitle" />
+      </div>
+      <div class="mb-3">
+        <label for="" class="form-label"> Content </label>
+        <textarea
+          type="text"
+          class="form-control"
+          rows="5"
+          v-model="content"
+        ></textarea>
+      </div>
+      <div class="col">
+        <div class="mb-3">
+          <label for="" class="form-label">Link text</label>
+          <input type="text" class="form-control" v-model="linkText" />
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label"> Link URL </label>
+          <input type="text" class="form-control" v-model="linkUrl" />
+        </div>
+        <div class="row mb-3">
+          <div class="form-check">
+            <input
+              type="checkbox"
+              class="form-check-input"
+              v-model="published"
+            />
+            <label for="gridCheck1" class="form-check-label">Published </label>
+          </div>
+        </div>
+      </div>
+      <div class="mb-3">
+        <button
+          class="btn btn-primary"
+          :disabled="isFormInValid"
+          @click.prevent="sumbitForm"
+        >
+          Create Page
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ["pageCreated"],
-    computed: {
-        isFormValid() {
-            return;
+  props: ["pageCreated"],
+  computed: {
+    isFormInValid() {
+      return (
+        !this.pageTitle || !this.content || !this.linkText || !this.linkUrl
+      );
+    },
+  },
+  data() {
+    return {
+      pageTitle: "",
+      content: "",
+      linkText: "",
+      linkUrl: "",
+      published: true,
+    };
+  },
+  methods: {
+    sumbitForm() {
+      if (!this.pageTitle || !this.content || !this.linkText || !this.linkUrl) {
+        alert("Please fill the form");
+        return;
+      }
+
+      this.$emit("pageCreated", {
+        pageTitle: this.pageTitle,
+        content: this.content,
+        link: {
+          text: this.linkText,
+          url: this.linkUrl,
         },
+        published: this.published,
+      });
+
+      this.pageTitle = "";
+      this.content0 = "";
+      this.linkText = "";
+      this.linkUrl = "";
+      this.published = true;
     },
-    data() {
-        return {
-            pageTitle: "",
-            content: "",
-            linkText: "",
-            linkUrl: "",
-        };
+  },
+  watch: {
+    pageTitle(newTitle, oldTitle) {
+      if (this.linkText === oldTitle) {
+        this.linkText = newTitle;
+      }
     },
-    methods: {
-        sumbitForm() {
-            if (
-                !this.pageTitle ||
-                !this.content ||
-                !this.linkText ||
-                !this.linkUrl
-            ) {
-                alert("Please fill the form");
-                return;
-            }
-        },
-    },
+  },
 };
 </script>
