581373ee7dce9e2f31b8b19539f425eec2e2d189
Braulio95 <braulioestradaalfonso@hotmail.com>
2023-12-10 23:02:25 -0600

    2nd commit

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,48 @@
 <template>
     <navbar
-    :pages="pages"
-    :active-page="activePage"
-    :nav-link-click="(index)=>activePage = index"
+        :pages="pages"
+        :active-page="activePage"
+        :nav-link-click="(index)=>activePage = index"
     ></navbar>
-    <page-viewer
-    :page="pages[activePage]"
-    ></page-viewer>
+    <!-- <page-viewer
+        v-if="pages.length > 0"
+        :page="pages[activePage]"
+    ></page-viewer> -->
+
+    <create-page
+    :page-created="pageCreated"
+    >
+    </create-page>
 </template>
 
 
 <script>
 import PageViewer from './components/PageViewer.vue';
 import Navbar from './components/Navbar.vue';
+import CreatePage from './components/CreatePage.vue';
     export default{
         components:{
             Navbar,
-            PageViewer
+            PageViewer,
+            CreatePage
+        },
+        created(){
+             this.getPages()
         },
         data(){
             return{
                 activePage:0,
-                pages: [
-                    {
-                        link: {text:'Home',url:'index.hmtl'},
-                        pageTitle: 'Home Page',
-                        content: 'This is the home content'
-                    },
-                    {
-                        link: {text:'About',url:'about.hmtl'},
-                        pageTitle: 'About Page',
-                        content: 'This is the About content'
-                    },
-                    {
-                        link: {text:'Contact',url:'contact.hmtl'},
-                        pageTitle: 'Contact Page',
-                        content: 'This is the Contact content'
-                    }
-                ]
+                pages: []
+            }
+        },
+        methods:{
+            async getPages(){
+                let res = await fetch('pages.json')
+                let data = await res.json();
+                this.pages = data;
+            },
+            pageCreated(pageObj){
+                console.log(pageObj)
             }
         }
     }
---
+++ src/components/CreatePage.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="container mb-3">
+        <form action="">
+            <div class="mb-3">
+                <label for="" class="form-label"> Page Title </label>
+                <input type="text" class="form-control" v-model="pageTitle" />
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label"> Content </label>
+                <textarea
+                    type="text"
+                    class="form-control"
+                    rows="5"
+                    v-model="content"
+                ></textarea>
+            </div>
+            <div class="col">
+                <div class="mb-3">
+                    <label for="" class="form-label">Link text</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="linkText"
+                    />
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label"> Link URL </label>
+                    <input type="text" class="form-control" v-model="linkUrl" />
+                </div>
+                <div class="row mb-3">
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" />
+                        <label for="gridCheck1" class="form-check-label"
+                            >Published
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div class="mb-3">
+                <button
+                    class="btn btn-primary"
+                    :disabled="isFormValid"
+                    @click.prevent="
+                        pageCreated({ pageTitle, content, linkText, linkUrl })
+                    "
+                >
+                    Create Page
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["pageCreated"],
+    computed: {
+        isFormValid() {
+            return;
+        },
+    },
+    data() {
+        return {
+            pageTitle: "",
+            content: "",
+            linkText: "",
+            linkUrl: "",
+        };
+    },
+    methods: {
+        sumbitForm() {
+            if (
+                !this.pageTitle ||
+                !this.content ||
+                !this.linkText ||
+                !this.linkUrl
+            ) {
+                alert("Please fill the form");
+                return;
+            }
+        },
+    },
+};
+</script>
